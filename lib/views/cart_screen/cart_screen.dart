a2606fef3c937ce197b2250a02fb3ab413256fff
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-03-05 00:57:00 +0530

    making cart screen a empty screen for now

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -6,7 +6,8 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
+      color: Colors.white,
+      child: "Cart is empty!".text.fontFamily(semibold).color(darkFontGrey).makeCentered(),
     );
   }
 }
