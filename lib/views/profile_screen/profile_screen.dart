d2a908aacad67dc06452b549d3c2cd547242ffce
Shamimkhan1406 <shamim4khan0@gmail.com>
2025-03-06 23:35:04 +0530

    adding dummy user detail to the profile screen

--- lib/views/profile_screen/profile_screen.dart
+++ lib/views/profile_screen/profile_screen.dart
@@ -1,12 +1,64 @@
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widget_common/bg_widget.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.purple,
+    return bgWidget(
+      Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                //edit profile button
+                const Align(
+                  alignment: Alignment.topRight,
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  ),
+                ).onTap(() {
+                  //
+                }),
+
+                //user detail section
+                Row(
+                  children: [
+                    Image.asset(
+                      imgProfile2,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ).box.roundedFull.clip(Clip.antiAlias).make(),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          "Dummy User".text.fontFamily(semibold).white.make(),
+                          5.heightBox,
+                          "dummyuser@gmail.com".text.white.make(),
+                        ],
+                      ),
+                    ),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: "logout".text.white.fontFamily(semibold).make(),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
